3531dcb2d66308e5d70fbe11faddfc4f1dfbd268
hy17304018 <35793804+hy17304018@users.noreply.github.com>
2018-09-11 14:25:20 +0900

    更新

--- js/start.js
+++ js/start.js
@@ -1,5 +1,10 @@
 var start = document.getElementById("start");
+var score =document.getElementById("score");
 
 function startFunction() {
 	start.innerHTML = "<button id='startButton'>Normal</button><br><button id='startButton' name='GameSpeed' value='fast'>Fast</button>";
 }
+
+function scoreFunction(){
+	score.innerHTML ="<button id='score'>Normal</button><br><button id='score'>Fast</button>";
+	} 
